cd3ee0a93b2b6a92d13eb359d27cb965836563df
Kate Zabolotskih <zabolotskihkate@gmail.com>
2020-09-02 01:35:13 +0300

    added lidar struct

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -15,53 +15,55 @@ using namespace boost::asio;
 using boost::system::error_code;
 io_service service;
 
-size_t read_complete(char * buf, const error_code & err, size_t bytes) {
-    if ( err) return 0;
-    bool found = std::find(buf, buf + bytes, '\n') < buf + bytes;
-    // we read one-by-one until we get to enter, no buffering
-    return found ? 0 : 1;
+std::ostream& operator<<(std::ostream & out, const Measure & measure)
+{
+    return out << measure.timestamp
+               << " " << measure.lidar1_data
+               << " " << measure.lidar2_data
+               << std::endl;
+}
+std::istream& operator>>(std::istream & in, Measure & measure)
+{
+    return in >> measure.timestamp >> measure.lidar1_data >> measure.lidar2_data;
 }
 
 ip::tcp::endpoint ep( ip::address::from_string("127.0.0.1"), 8001);
-void sync_echo(std::string msg) {
-    msg += "\n";
+
+void sync_echo(Measure & measure) {
+    streambuf bufOut, bufIn;
+    Measure measure1;
+
     ip::tcp::socket sock(service);
     sock.connect(ep);
-    sock.write_some(buffer(msg));
-    char buf[1024];
-    int bytes = read(sock, buffer(buf), boost::bind(read_complete,buf,_1,_2));
-    std::string copy(buf, bytes - 1);
-    msg = msg.substr(0, msg.size() - 1);
-    std::cout << "server echoed our " << msg << ": "
-              << (copy == msg ? "OK" : "FAIL") << std::endl;
+
+    std::ostream out(&bufOut);
+    out << measure;
+    write(sock, bufOut);
+
+    read_until(sock, bufIn, "\n");
+    std::istream in(&bufIn);
+    in >> measure1;
+
+
+    std::cout << "server echoed our " << measure1 << ("OK") << std::endl;
     sock.close();
 }
 
 int main(int argc, char* argv[]) {
     // connect several clients
-    std::string str1 = "John says hi";
-    char *cstr1 = new char[str1.length() + 1];
-    strcpy(cstr1, str1.c_str());
+    Measure measure1{123, 12.3,5.4},
+            measure2{124, 14.3, 13.22222},
+            measure3{125, 8.4429438274, 7.2323};
 
-    std::string str2 = "so does James";
-    char *cstr2 = new char[str2.length() + 1];
-    strcpy(cstr2, str2.c_str());
 
-    std::string str3 = "Boost.Asio is Fun!";
-    char *cstr3 = new char[str3.length() + 1];
-    strcpy(cstr3, str3.c_str());
-
-    char* messages[] = { cstr1, cstr2, cstr3, nullptr };
+    Measure* messages[] = { &measure1, &measure2, &measure3, nullptr };
 
     boost::thread_group threads;
-    for ( char ** message = messages; *message; ++message) {
-        threads.create_thread( boost::bind(sync_echo, *message));
+    for ( Measure ** message = messages; *message; ++message) {
+        threads.create_thread( boost::bind(sync_echo, **message));
         boost::this_thread::sleep( boost::posix_time::millisec(100));
     }
     threads.join_all();
-    delete [] cstr1;
-    delete [] cstr2;
-    delete [] cstr3;
 }
 
 
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -15,31 +15,37 @@ using namespace boost::posix_time;
 using boost::system::error_code;
 
 io_service service;
-size_t read_complete(Measure *measure, const error_code &err, size_t bytes) {
-    if (err) return 0;
-    bool found = std::find(buff, buff + bytes, '\n') < buff + bytes;
-    // we read one-by-one until we get to enter, no buffering
-    return found ? 0 : 1;
+
+std::ostream& operator<<(std::ostream & out, const Measure & measure)
+{
+    return out << measure.timestamp
+                << " " << measure.lidar1_data
+                << " " << measure.lidar2_data
+                << std::endl;
 }
-
-void readAllData(Measure& measure, ip::tcp::socket *sock) {
-    int bytes = read(*sock, sizeof(Measure), boost::bind(read_complete, measure, _1,_2));
-
+std::istream& operator>>(std::istream & in, Measure & measure)
+{
+    return in >> measure.timestamp >> measure.lidar1_data >> measure.lidar2_data;
 }
 
 void handle_connections() {
     ip::tcp::acceptor acceptor(service, ip::tcp::endpoint(ip::tcp::v4(),8001));
-    char buff[1024];
+
     while (true) {
         ip::tcp::socket sock(service);
         acceptor.accept(sock);
-        Measure *measure;
 
-        int bytes = read(sock, buffer(measure, sizeof(Measure)),
-                         boost::bind(read_complete,buff,_1,_2));
-        std::cout << buff << std::endl;
-        std::string msg(buff, bytes);
-        sock.write_some(buffer(msg));
+        streambuf bufIn, bufOut;
+        Measure measure;
+
+        read_until(sock, bufIn, "\n");
+        std::istream in(&bufIn);
+        in >> measure;
+
+        std::cout << measure;
+        std::ostream out(&bufOut);
+        out << measure << std::endl;
+        write(sock, bufOut);
         sock.close();
     }
 }
